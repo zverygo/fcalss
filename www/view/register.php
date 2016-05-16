f0a542d17d3d4b1a0cc31c7f7413889cb4e82eb4
zverygo <simogin.roman@gmail.com>
2016-05-16 14:49:52 +0300

    сделал фундамент для шаблона регистрации

---
+++ www/view/register.php
@@ -0,0 +1,27 @@
+<?php include_once 'header.php'; ?>
+
+<div class="container">
+    <h5> ЗАРЕГЕСТРИРУЙСЯ ВО МНЕ ПОЛНОСТЬЮ </h5>
+    <form method="post">
+        <div class="form-group">
+            <label>E-MAIL</label>
+            <br>
+            <input type="email" name="email" class="form-item" value="" autofocus required>
+        </div>
+        <div class="form-group">
+            <label>PASSWORD</label>
+            <br>
+            <input type="password" name="passwodr" class="form-item" value="" required>
+        </div>
+        <!-- ЗАГОТОВКА ДЛЯ ПОЛЯ С ПРОВЕРКОЙ ПАРОЛЯ
+        <div class="form-group">
+            <label>REPEAT PASSWORD</label>
+            <br>
+            <input type="password" name="r_password" class="form-item" value="" required>
+        </div>
+        -->
+        <input type="submit" value="REGISTER" class="btn">
+    </form>
+</div>
+    
+<?php include_once 'footer.php'; ?>
