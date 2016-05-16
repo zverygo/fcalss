f95c09f3b5bf5cf065cd39ec5b99b0d8198764f0
zverygo <simogin.roman@gmail.com>
2016-05-16 13:23:32 +0300

    сделал шаблоны для футера и хедера, теперь просто их вставляем в остальные шаблоны

---
+++ www/view/header.php
@@ -0,0 +1,12 @@
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>F_class</title>
+    <link rel="stylesheet" href="../style/style.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+    
+    <div class="container">
+        <h1><a href="../index.php">BLOG</a></h1>
+    </div>
+</head>
+<body>
