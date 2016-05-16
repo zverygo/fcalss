be817510b6569f497da33620d27377630a6d604d
zverygo <simogin.roman@gmail.com>
2016-05-16 13:25:06 +0300

    подмарафетил, подключил футер и хедер вешними шаблонами

--- www/view/adm_article.php
+++ www/view/adm_article.php
@@ -1,33 +1,24 @@
-<html>
-<head>
-    <meta charset="utf-8">
-    <title>F_class</title>
-    <link rel="stylesheet" href="../style/style.css">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
-</head>
-<body>
-    <div class="container">
-        <h1><a href="../index.php">BLOG</a></h1>
-        <div>
+<?php include_once 'header.php'; ?>
+
+        <div class="container">
             <form method="post" action="../admin/admin.php?action=<?=$_GET['action']?>&id=<?=$_GET['id']?>">
                 <div class="form-group">
                     <label>Название</label>
+                    <br>
                     <input type="text" name="title" value="<?=$text['title']?>" class="form-item" autofocus required>
                 </div>
                 <div class="form-group">
                     <label>Дата</label>
+                    <br>
                     <input type="date" name="date" value="<?=$text['date']?>" class="form-item" required>
                 </div>
                 <div class="form-group">
                     <label>Содержание</label>
+                    <br>
                     <textarea class="form-item" name="content" required><?=$text['content']?></textarea>
                 </div>
                 <input type="submit" value="Save" class="btn">
             </form> 
         </div>
-        <footer>
-            <p>BLOG <br> Coryright &copy; 2016</p>
-        </footer>
-    </div>
-</body>
-</html>
+
+<?php include_once 'footer.php'; ?>
--- www/view/adm_articles.php
+++ www/view/adm_articles.php
@@ -1,36 +1,22 @@
-<html>
-<head>
-    <meta charset="utf-8">
-    <title>F_class</title>
-    <link rel="stylesheet" href="../style/style.css">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
-</head>
-<body>
+<?php include_once 'header.php'; ?>
+
     <div class="container">
-        <h1><a href="../index.php">BLOG</a></h1>
-        <div>
-            <a href="admin.php?action=add">Добавить статью</a><br><br>
-            <table class="admin-table">
-                <tr>
-                    <th>Дата</th>
-                    <th>Заголовок</th>
-                    <th></th>
-                    <th></th>
-                </tr>
-                <?php foreach($text as $a): ?>
-                <tr>
-                    <td><?=$a['date']?></td>
-                    <td><?=$a['title']?></td>
-                    <td><a href="../admin/admin.php?action=edit&id=<?=$a['id']?>">Редактировать</a></td>
-                    <td><a href="../admin/admin.php?action=delete&id=<?=$a['id']?>">Удалить</a></td>
-                </tr>
-            
-                <?php endforeach ?>
-            </table>    
-        </div>
-        <footer>
-            <p>BLOG <br> Coryright &copy; 2016</p>
-        </footer>
+        <a href="admin.php?action=add">Добавить статью</a><br><br>
+        <table class="admin-table">
+            <tr>
+                <th>Дата</th>
+                <th>Заголовок</th>
+                <th></th>
+                <th></th>
+            </tr>
+            <?php foreach($text as $a): ?>
+            <tr>
+                <td><?=$a['date']?></td>
+                <td><?=$a['title']?></td>
+                <td><a href="../admin/admin.php?action=edit&id=<?=$a['id']?>">Редактировать</a></td>
+                <td><a href="../admin/admin.php?action=delete&id=<?=$a['id']?>">Удалить</a></td>
+            </tr>
+        <?php endforeach ?>
+        </table>    
     </div>
-</body>
-</html>
+<?php include 'footer.php'; ?>
