28d3df4997a12f3ef542cd312480ed5517112ae4
zverygo <simogin.roman@gmail.com>
2016-05-20 17:57:50 +0300

    шаблон личного кабинета

--- www/view/user.php
+++ www/view/user.php
@@ -1,8 +1,48 @@
 <?php include_once 'header.php'; ?>
 
 <div class="container">
-    <h3>USER <?=$date = date("m.d.y");?></h3>
+    
+    <h3>Данные пользователя</h3>
+    <table class="admin-table">
+        <tr>
+            <th>E-mail</th>
+            <td><?=$text_2['email']?></td>
+        </tr>
+        <tr>
+            <th>Role</th>
+            <td><?=$text_2['role']?></td>
+        </tr>
+        <tr>
+            <th>Date Reg</th>
+            <td><?=$text_2['date_reg']?></td>
+        </tr>
+        <tr>
+            <th>About</th>
+            <td><?=$text_2['About']?></td>
+        </tr>        
+    </table>    
 </div>   
 
-<?php include_once 'footer.php'; ?>
+<? if ($_SESSION['role'] == "moderator") :?>
+<div class="container">
+    <h3>Tабличтка с постами если модератор</h3>
+    <table class="admin-table">
+        <tr>
+            <th>Дата и время</th>
+            <th>Заголовок</th>
+            <th></th>
+            <th></th>
+        </tr>
+        <?php foreach($text as $a): ?>
+        <tr>
+            <td><?=$a['date']?></td>
+            <td><?=$a['title']?></td>
+            <td><a href="../admin/admin.php?action=edit&id=<?=$a['id']?>">Редактировать</a></td>
+            <td><a href="../admin/admin.php?action=delete&id=<?=$a['id']?>">Удалить</a></td>
+        </tr>
+        <?php endforeach ?>
+    </table>    
+</div>   
+<? endif ?>
 
+<?php include_once 'footer.php'; ?>
