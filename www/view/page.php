6884f4b6c15a5988946874c27a9711abc22ad466
zverygo <simogin.roman@gmail.com>
2016-06-21 09:53:29 +0300

    добавил вывод в боковой блок самого популярного поста

--- www/view/page.php
+++ www/view/page.php
@@ -30,40 +30,41 @@
                     $row = new Post (HOST, USER, PASS, DB);
                     $num_row = $row -> get_num_row_db ();
                     $lim = 3;
-                    $num_pages = $num_row/$lim;
-                    $pages = 6;
+                    $num_pages = (int) ($num_row/$lim);
+                    $pages = 5;
                     $z=5;
+                    $block = (int) (($num_pages/$pages)+1);
                     if (!isset($_GET['page'])) {
-                        for ($a = 1; $a < $pages; $a++) {
+                        for ($a = 1; $a <= $pages; $a++) {
                             echo '<li><a href="index.php?page='.$a.'&qqq='.$a.'" >'.$a.'</a></li>';
                         }
                     }
                     else {
                         $n_p = $_GET['page'];
-                        if ($_GET['page'] < 4){
-                            for ($a = 1; $a < $pages; $a++) {
+                        if ($_GET['page'] <= 4){
+                            for ($a = 1; $a <= $pages; $a++) {
                                 echo '<li><a href="index.php?page='.$a.'&qqq='.$a.'" >'.$a.'</a></li>';
                             }
                         }
                         else if ($_GET['page'] > ($num_pages-$lim)){
-                            for ($a = 1; $a < $pages; $a++) {
+                            for ($a = 1; $a <= $pages; $a++) {
                                 $z-=1;
                                 echo '<li><a href="index.php?page='.($num_pages-$z).'&qqq='.$a.'" >'.($num_pages-$z).'</a></li>';
                             }
-                        }
-                        else if ($_GET['page'] == 8 && $_GET['qqq'] >3){
-                            for ($a = 1; $a < $pages; $a++) {
+                        }//////////////////////////////////
+                        else if ($_GET['page'] >= ($num_pages-3) && $_GET['qqq'] >3){
+                            for ($a = 1; $a <= $pages; $a++) {
                                 $z-=1;
                                 echo '<li><a href="index.php?page='.($num_pages-$z).'&qqq='.$a.'" >'.($num_pages-$z).'</a></li>';
                             }
-                        }
+                        }////////////////////////////////////
                         else if ($_GET['page'] == 4 && $_GET['qqq'] < 3){
-                            for ($a = 1; $a < $pages; $a++) {
+                            for ($a = 1; $a <= $pages; $a++) {
                                 echo '<li><a href="index.php?page='.$a.'&qqq='.$a.'" >'.$a.'</a></li>';
                             }
                         }
                         else if ($_GET['page']>=4){
-                            for ($a = 1; $a < $pages; $a++) {
+                            for ($a = 1; $a <= $pages; $a++) {
                                 if ($_GET['qqq'] > 3) {
                                     echo '<li><a href="index.php?page='.($n_p+$a).'&qqq='.$a.'" >'.($n_p+$a).'</a></li>';
                                 }
@@ -83,12 +84,31 @@
             
         </div>
         <div class="col-lg-3 col-md-3 hidden-sm hidden-xs">
-            <br><br>
-            <img src="http://placehold.it/250x150">
-            <br><br>
-            <img src="http://placehold.it/250x250">
-            <br><br>
-            <img src="http://placehold.it/250x350">
+            <div>
+                <BR>
+                <h4>BEST POST</h4>
+                <?php
+                    $row = new Post (HOST, USER, PASS, DB);
+                    $b_post = $row -> best_post ();
+                ?>
+                <h4><a href="index.php?id=<?=$b_post['id']?>"><?=$b_post['title']?></a></h4>
+                <em>Опубликовано: <?=$b_post['date']?></em>
+                <p><?=$b_post['content']?></p>
+            </div>
+            <div>
+                <br>
+                <h4>ПОПУЛЯРНЫЕ РАЗДЕЛЫ</h4>
+                <?php
+                    $row = new Post (HOST, USER, PASS, DB);
+                    $test_2 = $row -> pop_post ();
+                       
+                       
+                ?>
+            </div>
+            <div>
+                <br>
+                <img src="http://placehold.it/250x350">
+            </div>
         </div>      
     </div>
 </div>
